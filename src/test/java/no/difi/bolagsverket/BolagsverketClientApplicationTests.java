8f7ae4ed6b4ba64f56ea5e54dca26b332d4ef0e3
Johannes Molland <jom@difi.no>
2018-03-07 18:22:29 +0100

    Adds mock properties to fix failing spring boot test.

--- src/test/java/no/difi/bolagsverket/BolagsverketClientApplicationTests.java
+++ src/test/java/no/difi/bolagsverket/BolagsverketClientApplicationTests.java
@@ -3,14 +3,16 @@ package no.difi.bolagsverket;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-mock.properties")
 public class BolagsverketClientApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
