c51b3a021c55fbe4b2fb18f8bf7f0d1c77394d6e
Johannes Molland <jom@difi.no>
2018-03-08 08:51:11 +0100

    Adds validator field to controller.

--- src/main/java/no/difi/bolagsverket/api/BolagsverketController.java
+++ src/main/java/no/difi/bolagsverket/api/BolagsverketController.java
@@ -1,5 +1,6 @@
 package no.difi.bolagsverket.api;
 
+import no.difi.bolagsverket.service.ValidatorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,11 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class BolagsverketController {
 
+    private final ValidatorService validatorService;
+
+    public BolagsverketController(ValidatorService validatorService) {
+        this.validatorService = Objects.requireNonNull(validatorService);
+    }
+
     @RequestMapping(value = "/identifier/{id}", method = RequestMethod.GET)
     public Object getIdentifier(@PathVariable String id) {
-        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        boolean isValid = validatorService.validate(id);
+        HttpStatus status = isValid ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND;
+        return new ResponseEntity<Void>(status);
     }
 }
--- src/test/java/no/difi/bolagsverket/api/BolagsverketControllerTest.java
+++ src/test/java/no/difi/bolagsverket/api/BolagsverketControllerTest.java
@@ -1,13 +1,17 @@
 package no.difi.bolagsverket.api;
 
+import no.difi.bolagsverket.service.ValidatorService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,9 +23,18 @@ public class BolagsverketControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ValidatorService validatorServiceMock;
+
     @Test
     public void testGetIdentifier_identifierExists_shouldReturn204() throws Exception {
+        when(validatorServiceMock.validate(anyString())).thenReturn(true);
         mockMvc.perform(get("/identifier/162021005489")).andExpect(status().isNoContent());
     }
 
+    @Test
+    public void testGetIdentifier_identifierDoesNotExist_shouldReturn404() throws Exception {
+        when(validatorServiceMock.validate(anyString())).thenReturn(false);
+        mockMvc.perform(get("/identifier/162021005489")).andExpect(status().isNotFound());
+    }
 }
