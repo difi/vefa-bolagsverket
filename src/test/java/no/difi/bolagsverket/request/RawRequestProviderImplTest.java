4d06e351c41e4f67ae4630aec3098a65bbc7da70
Johannes Molland <jom@difi.no>
2018-03-13 12:22:41 +0100

    Adds validation of generated XML request in unit test.

--- src/test/java/no/difi/bolagsverket/request/RawRequestProviderImplTest.java
+++ src/test/java/no/difi/bolagsverket/request/RawRequestProviderImplTest.java
@@ -1,16 +1,31 @@
 package no.difi.bolagsverket.request;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@Slf4j
 public class RawRequestProviderImplTest {
 
     private RawRequestProviderImpl target;
+    private final String BOLAGSVERKET_IDENTIFIER = "2021005489";
 
     @Before
     public void setUp() {
@@ -23,9 +38,28 @@ public class RawRequestProviderImplTest {
     }
 
     @Test
-    public void testGetRequest_organizationNumberIsValid_shouldReturnNonNull() {
-        Optional<String> result = target.getRequest("162021005489");
+    public void testGetRequest_organizationNumberIsValid_shouldValidateAndReturnNonNull() {
+        Optional<String> result = target.getRequest(BOLAGSVERKET_IDENTIFIER);
         assertTrue(result.isPresent());
-        assertFalse(result.get().isEmpty());
+        String xml = result.get();
+        assertFalse(xml.isEmpty());
+        assertTrue(validateXml(xml));
+    }
+
+    private boolean validateXml(String xml) {
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            File schemaFile = new ClassPathResource("/xsd/foretagsinformation/Foretagsfraga_1.1.xsd").getFile();
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            Source xmlSource = new StreamSource(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)));
+            validator.validate(xmlSource);
+        } catch (SAXException e) {
+            log.info("XML validation failed with message: {}", e.getMessage());
+            return false;
+        } catch (IOException e) {
+            throw new IllegalStateException("The request could not be validated.", e);
+        }
+        return true;
     }
 }
