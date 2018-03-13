e9c83b28aeb66fdbaa462cf93b9f9f1800017728
Johannes Molland <jom@difi.no>
2018-03-13 12:23:30 +0100

    Adds required SchemaVersion attribute in generated XML request.

--- src/main/java/no/difi/bolagsverket/request/RawRequestProviderImpl.java
+++ src/main/java/no/difi/bolagsverket/request/RawRequestProviderImpl.java
@@ -22,12 +22,14 @@ class RawRequestProviderImpl implements RequestProvider {
 
     private static final String PRODUKT_VERSION = "2.00";
     private static final String PRODUKT_NAME = "F1Grundpaket";
+    private static final String SCHEMA_VERSION = "1.1";
 
     @Override
     public Optional<String> getRequest(String identifier) {
         Objects.requireNonNull(identifier);
-        log.debug("Building XML request for identifier '{}'.", identifier);
+        log.debug("Building xmlFraga for identifier '{}'.", identifier);
         String serializedQuery = serializeQuery(getForetagsfraga(identifier));
+        log.debug(serializedQuery);
         return null != serializedQuery
                 ? Optional.of(serializedQuery)
                 : Optional.empty();
@@ -38,6 +40,7 @@ class RawRequestProviderImpl implements RequestProvider {
         try (StringWriter writer = new StringWriter()) {
             JAXBContext context = JAXBContext.newInstance(Foretagsfraga.class);
             Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
             marshaller.marshal(query, writer);
             serializedQuery = writer.toString();
         } catch (JAXBException e) {
@@ -54,6 +57,7 @@ class RawRequestProviderImpl implements RequestProvider {
         return Foretagsfraga.builder()
                 .withInformationshuvud(header)
                 .withProdukt(produkt)
+                .withSchemaVersion(SCHEMA_VERSION)
                 .build();
     }
 
